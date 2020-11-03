7c7fbb9ffb9e8d1d149dade1258ccca38f850822
rodrigarc <rodrigo.arcoverdi@gmail.com>
2020-11-03 17:00:59 +0100

    comparings dbs improvement

--- 2020-10-30_public-db-comparison.R
+++ 2020-10-30_public-db-comparison.R
@@ -24,23 +24,24 @@ ggplot(tabs_v_s, aes(sample=V_SHM)) +
 tabs_v_s %>% group_by(.id) %>% shapiro_test(V_SHM)
 
 
-tabs_v_s <- tabs_v_s %>% mutate(pdb = ifelse(grepl("individualized", .id), "individualized", "public"))
+tabs_v_s <- tabs_v_s %>% mutate(pdb = ifelse(grepl("Individualized", .id), "Individualized", "Public"))
 
 stat.test <- tabs_v_s %>% t_test(V_SHM ~ .id, p.adjust.method = "holm") 
-
-tabs_v_s$.id <- sub("-filtered.tab", "",x = tabs_v_s$.id)
-
-tabs_v_s$.id <- factor(tabs_v_s$.id, levels = c( "IMGThm","IMGTrm", 
-                                                 "ramesh", "Cirelli", 
-                                                 "sundling", "Corcoran", 
-                                                 "Francica", "publicdb", "mmul10",  "zhang",
-                                                "individualized.tab"))
-tabs_v_s %>% group_by(.id) %>% summarise(avg = mean(V_SHM)) %>% arrange(avg)
+View(stat.test)
+tabs_v_s$.id <- sub("-filtered.tab|\\.tab", "",x = tabs_v_s$.id)
+unique(tabs_v_s$.id)
+tabs_v_s$.id <- factor(tabs_v_s$.id, levels = c( "IMGThs","IMGTrm", 
+                                                 "Ramesh", "Cirelli", 
+                                                 "Sundling", "Corcoran", 
+                                                 "Francica", "Mmul10",  "Zhang", "Public", "Nestor",
+                                                "Individualized"))
+tabs_v_s %>% group_by(.id) %>% summarise(avg = median(V_SHM)) %>% arrange(avg)
 
 
 
 # Create the plot
-
+library("ggsci")
+pal_npg()
 myplot <- ggviolin(
   tabs_v_s, x = ".id", y = "V_SHM",
   fill = ".id", palette = "npg", legend = "none",
@@ -49,17 +50,17 @@ myplot <- ggviolin(
 myplot
 
 # Add statistical test p-values
-stat.test <- stat.test %>% add_xy_position(x = ".id") %>% filter(group1 == "individualized.tab"|group2 == "individualized.tab") %>% 
+stat.test <- stat.test %>% add_xy_position(x = ".id") %>% filter(group1 == "Individualized"|group2 == "Individualized") %>% 
   as.data.frame()
 class(stat.test)
-#write.csv(stat.test[-c(11:14)], "~/Box Sync/RSV NGS/Database comparison/plot/2020-02-20_t-test_p-adjusted.csv",
+#write.csv(stat.test, "~/Box Sync/RSV NGS/Database comparison/plot/2020-11-03//2020-11-03_t-test_p-adjusted.csv",
           row.names = F)
 
 
 myplot + stat_pvalue_manual(stat.test, label = "p.adj.signif", bracket.nudge.y = -15) +
   rotate_x_text(angle = 65)
 
-
+stat.test
 
 ##create a new public_db
 id <- list.files("~/Box Sync/RSV NGS/Database comparison/")
--- 2020-11-02_combing-individualized-filtered-tabs.R
+++ 2020-11-02_combing-individualized-filtered-tabs.R
@@ -4,8 +4,7 @@ ls <- list.files("~/Box Sync/RSV NGS/v4_Analysis/v4_Germlines/combined", full.na
 
 ls <- ls[-c(1,11)]
 dna <- lapply(ls, readDNAStringSet)
-dna_c <- lapply(dna, c)
-dna_c <- do.call(c,)
+dna_c <- do.call(c,dna)
 
 id <- c("E11","E12","E14","E16","E17","E18","E21","E23","E24")
 
@@ -22,4 +21,18 @@ tabs_comb_sel <- tabs_comb %>% select(.id, name, V_SHM)
 fwrite(tabs_comb, "~/Box Sync/RSV NGS/Database comparison/filtered_tabs/individualized.tab",
              quote = F)
 
+#####combing public  
+ls <- list.files("~/Box Sync/RSV NGS/Database comparison/Public/Public_without-nestor", full.names = T)
+ls_names <- list.files("~/Box Sync/RSV NGS/Database comparison/Public/Public_without-nestor")
+dna <- lapply(ls, readDNAStringSet)
+names(dna) <- sub(".fasta","",ls_names)
 
+for(i in names(dna)){
+  names(dna[[i]]) <- paste0(i,"_",names(dna[[i]]))
+}
+names(dna) <- NULL
+dna_c <- do.call(c, dna)
+writeXStringSet(unique(dna_c), 
+                "~/Box Sync/RSV NGS/Database comparison/Public/Public_without-nestor/V.fasta")
+
+                
